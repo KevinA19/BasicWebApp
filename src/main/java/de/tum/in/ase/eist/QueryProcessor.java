68590a0920d44a3a5ff9fec527656aa311f3f9dd
Kevin Arenja <arenja.kevin@gmx.de>
2022-07-14 15:15:50 +0200

    Question: what is 10 plus 15

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -7,9 +7,6 @@ public class QueryProcessor {
 
     public String process(String query) {
 		query = query.toLowerCase();
-        int summand1 = Integer.parseInt(query.substring(9,10));
-        int summand2 = Integer.parseInt(query.substring(17,18));
-        int summe = summand1 + summand2;
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
@@ -17,6 +14,9 @@ public class QueryProcessor {
         } else if (query.contains("name")) {
             return "MyTeam";
         } else if (query.contains("what is 10 plus 15")) {
+            int summand1 = Integer.parseInt(query.substring(9,10));
+            int summand2 = Integer.parseInt(query.substring(17,18));
+            int summe = summand1 + summand2;
             return String.valueOf(summe);
         }
         else { // TODO extend the programm here
