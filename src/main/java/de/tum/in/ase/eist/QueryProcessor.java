ae425ce33871225a87718e8dde6fa8ddf7981b85
Kevin Arenja <arenja.kevin@gmx.de>
2022-07-14 15:47:06 +0200

    Question: which of the following numbers is the largest and what is x plus y. gelöst

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,29 +6,41 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        } else if(query.contains("which of the following numbers is the largest:")){
+            query = query.substring(query.lastIndexOf(":"));
+            query = query.replaceAll("[^0-9]", " ");
+            query = query.replaceAll("\\s+", " ");
+            query = query.trim();
+            String[] bitches = query.split(" ");
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i< bitches.length;i++){
+                if(max<Integer.parseInt(bitches[i])){
+                    max = Integer.parseInt(bitches[i]);
+                }
+            }
+            return "" + max;
+
+
+        } else if (query.contains("what is")) {
+            query = query.substring(query.lastIndexOf(":"));
+            query = query.replaceAll("[^0-9]", " ");
+            query = query.replaceAll("\\s+", " ");
+            query = query.trim();
+            String[] bitches = query.split(" ");
+            int sum=0;
+            for (int i=0;i< bitches.length;i++){
+                sum += Integer.parseInt(bitches[i]);
+            }
+            return ""+sum;
+
         } else if (query.contains("name")) {
             return "MyTeam";
-        } else if (query.contains("what is")) {
-            int summand1 = Integer.parseInt(query.substring(8,10));
-            int summand2 = Integer.parseInt(query.substring(16,18));
-            int summe = summand1 + summand2;
-            return String.valueOf(summe);
-        }
-        else if (query.contains("which of")) {
-            int summand1 = Integer.parseInt(query.substring(47,49));
-            int summand2 = Integer.parseInt(query.substring(51,54));
-            if (summand1 > summand2) {
-                return String.valueOf(summand1);
-            } else {
-                return String.valueOf(summand2);
-            }
-        }
-        else { // TODO extend the programm here
+        } else { // TODO extend the programm here
             return "";
         }
     }
